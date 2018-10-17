40765c7c0daf21e2544083ffbd0c61c9840ec1eb
DaoDucKhiem <38343622+DaoDucKhiem@users.noreply.github.com>
2018-10-17 23:47:17 +0700

    bản đầy đủ các version

---
+++ Dictionary/src/controller/insertWord.java
@@ -0,0 +1,71 @@
+package controller;
+
+import Controller.Controller1;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author DUC KHIEM
+ */
+public class insertWord implements Initializable {
+    
+    @FXML
+    private TextField addWord;
+    @FXML
+    private TextField noun;
+    @FXML
+    private TextField verb;
+    @FXML
+    private TextField adjective;
+    @FXML
+    private TextField other;
+
+    private String wordExplain;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void Insert(ActionEvent event) {
+        wordExplain = Controller1.getDic().setExlain(noun.getText() , verb.getText() , adjective.getText(), other.getText());
+        Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+        alert1.setTitle("Insert Word");
+        if( Controller1.getDic().AddWord(addWord.getText(), wordExplain) )
+        {
+            alert1.setContentText("Add success!");
+            Controller1.getData().updateDataBase();
+        }
+        else alert1.setContentText("Add error!");       
+        alert1.show();
+        ((Stage) anchorPane.getScene().getWindow()).close();
+    }
+
+    
+    @FXML
+    private void Cancel(ActionEvent event) {
+        ((Stage) anchorPane.getScene().getWindow()).close();
+    }
+
+    public String getExplain() {
+        return "<html>" + wordExplain + "</html>";
+    }
+}
