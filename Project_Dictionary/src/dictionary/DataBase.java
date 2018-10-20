b54f775db2eca9166bf821c9c02d8115449cdd8e
DaoDucKhiem <38343622+DaoDucKhiem@users.noreply.github.com>
2018-10-20 20:55:23 +0700

    update lại

---
+++ Project_Dictionary/src/dictionary/DataBase.java
@@ -0,0 +1,31 @@
+package dictionary;
+
+import java.util.Collections;
+
+/**
+ *
+ * @author DUC KHIEM
+ */
+public class DataBase {
+    Dictionary dic = new Dictionary();
+    String nameFile;
+
+    public DataBase() {
+        setDataBase();      
+    }
+      
+    private void setDataBase() {
+        dic.setPath("data/E_V.html");
+        dic.readDataFromFile();
+    }
+    
+    public Dictionary getDataBase() {
+        return dic;
+        
+    }
+    public void updateDataBase() {
+        Collections.sort(dic.getKey());
+        dic.writeData();
+    }
+    
+}
